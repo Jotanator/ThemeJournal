ddf57a918089fd646ce9d0d2b3284400b60d5d45
DemPeeps <dhruvtummala@gmail.com>
2022-01-15 18:09:50 -0800

    Small text editing functionality

--- App.js
+++ App.js
@@ -1,13 +1,33 @@
+import React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class App extends React.Component {
+  constructor () {
+    super();
+    this.state = {editable: false, text: "This is the theme"};
+  }
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Theme</Text>
+        {this.state.editable ?
+          <TextInput
+            value={this.state.text}
+            onChangeText={(value) => this.setState({text: value})}
+            autoFocus
+            onBlur={() => this.setState({editable: false})}
+          /> :
+          <Text
+            onPress={() => this.setState({editable: true})}
+          >
+            {this.state.text}
+          </Text>
+        }
+        <StatusBar style="auto" />
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
